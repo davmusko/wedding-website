3b7ea1b0c02af58f6d38f558f1db1ebfdc5c4d97
David Musko <davmusko@gmail.com>
2020-05-23 00:38:37 -0400

    carousel done for bridesmaids

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,3 +6,16 @@ links.forEach(function (link) {
     document.querySelector("#nav-toggle").checked = false;
   });
 });
+
+//carousel
+$(document).ready(function () {
+    $("#testimonial-slider").owlCarousel({
+      items: 3,
+      itemsDesktop: [1000, 3],
+      itemsDesktopSmall: [979, 2],
+      itemsTablet: [768, 2],
+      itemsMobile: [650, 1],
+      pagination: true,
+      autoPlay: true,
+    });
+  });
